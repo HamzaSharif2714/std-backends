7f86a97efac4eb80f76cb14fc32b877bdc5fcb3b
HamzaSharif2714 <hamzasaeed.2714@gmai.com>
2023-02-01 18:43:55 +0500

    seven commit

--- models/googleModel.js
+++ models/googleModel.js
@@ -42,8 +42,6 @@ const googleSchema = new mongoose.Schema(
     },
     website: {
       type: String,
-      match:
-        /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
     },
     isPrivate: {
       type: Boolean,
